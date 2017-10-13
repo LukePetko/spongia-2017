950c8b76b102a4be9e366f2206256ff1efe92548
Lukas Petko <luke.petko@gmail.com>
2017-10-13 08:32:04 +0200

    ranná hodina spg

--- game/main.py
+++ game/main.py
@@ -3,7 +3,96 @@ from threading import *
 from random import *
 
 # from map import *
-from movement import *
+# from movement import *
+
+def bind_p1():
+    global pressed, game_canvas, level_map, player_one, player_two, box_side
+    for char in ["w", "s", "a", "d", "W", "S", "A", "D", "Down", "Up", "Left", "Right"]:
+        pressed[char] = False
+        print(char, pressed[char])
+    print(pressed)
+    game_canvas.bind("<KeyPress>", _pressed)
+    game_canvas.bind("<KeyRelease>", _released)
+
+    # game_canvas.bind_all("<Key>", lambda event, level_map = level_map, game_canvas = game_canvas, player_one = player_one, box_side = box_side, player_two = player_two: movement(event, level_map, game_canvas, player_one, player_two, box_side))
+    movement()
+
+def _pressed(event):
+    global pressed
+    pressed[event.keysym] = True
+
+def _released(event):
+    global pressed
+    pressed[event.keysym] = False
+
+def wait(wait_bool_p1):
+    wait_bool_p1 = True
+    print(wait_bool, "- wait function")
+
+def wait_p1():
+    global wait_bool_p1
+    wait_bool_p1 = True
+
+def wait_p2():
+    global wait_bool_p2
+    wait_bool_p2 = True
+
+
+def movement():
+    global wait_bool_p1, wait_bool_p2, t, level_map, game_canvas, player_one, player_two, box_side, pressed
+    game_canvas.tag_raise(player_one)
+    game_canvas.tag_raise(player_two)
+
+    if wait_bool_p1:
+        if pressed["s"]:
+            game_canvas.move(player_one, 0, 4)
+            wait_bool_p1 = False
+            game_canvas.after(t, wait_p1)
+            print(game_canvas.coords(player_one))
+        if pressed["w"]:
+            game_canvas.move(player_one, 0, -4)
+            wait_bool_p1 = False
+            # print(game_canvas.coords(player_one))
+            game_canvas.after(t, wait_p1)
+        if pressed["a"]:
+            game_canvas.move(player_one, -4, 0)
+            wait_bool_p1 = False
+            # print(game_canvas.coords(player_one))
+            game_canvas.after(t, wait_p1)
+        if pressed["d"]:
+            game_canvas.move(player_one, 4, 0)
+            wait_bool_p1 = False
+            # print(game_canvas.coords(player_one))
+            # print(wait_bool_p1, "- pri d")
+            game_canvas.after(t, wait_p1)
+
+    if wait_bool_p2:
+        if pressed["Down"]:
+            game_canvas.move(player_two, 0, 4)
+            print(game_canvas.coords(player_two))
+            wait_bool_p2 = False
+            game_canvas.after(t, wait_p2)
+        if pressed["Up"]:
+            game_canvas.move(player_two, 0, -4)
+            print(game_canvas.coords(player_two))
+            wait_bool_p2 = False
+            game_canvas.after(t, wait_p2)
+        if pressed["Left"]:
+            game_canvas.move(player_two, -4, 0)
+            print(game_canvas.coords(player_two))
+            wait_bool_p2 = False
+            game_canvas.after(t, wait_p2)
+        if pressed["Right"]:
+            game_canvas.move(player_two, 4, 0)
+            print(game_canvas.coords(player_two))
+            wait_bool_p2 = False
+            game_canvas.after(t, wait_p2)
+    game_canvas.after(10, movement)
+
+wait_bool_p1 = True
+wait_bool_p2 = True
+t = 12
+pressed = {}
 
 def shirt():
     global box_side, root, level_map, game_start, shirt_yellow, shirt_list
@@ -20,17 +109,15 @@ def shirt():
 
 
 def draw(level, root):
-    global game_canvas, box_side, level_map, shirt_yellow, shirt_list, shirt_count
+    global game_canvas, box_side, level_map, shirt_yellow, shirt_list, shirt_count, player_one, player_two
     level_file = open(level)
     print(level_file)
     level_map = level_file.readlines()
     box_side = root.winfo_screenheight() // len(level_map[0].strip().split(" "))
 
-    # print(len(level_map))
 
     for y in range(len(level_map)):
         level_map[y] = level_map[y].strip().split(" ")
-        # print(level_map[y], len(level_map[y]))
         for x in range(len(level_map[y])):
             if level_map[y][x] == "0":
                 game_canvas.create_rectangle(x * box_side, y * box_side, (x + 1) * box_side, (y + 1) * box_side, fill = "black")
@@ -42,14 +129,14 @@ def draw(level, root):
                 game_canvas.create_rectangle(x * box_side, y * box_side, (x + 1) * box_side, (y + 1) * box_side, outline = "red", fill = "red")
             elif level_map[y][x] == "p1":
                 game_canvas.create_rectangle(x * box_side, y * box_side, (x + 1) * box_side, (y + 1) * box_side, outline = "blue", fill = "blue")
-                # player_one = Label(root, height = box_side, width = int(0.8 * box_side), anchor = NW, fg = "black")
-                # player_one.place(x = (x * box_side + box_side // 10),y = (y * box_side))
-                player_one = game_canvas.create_rectangle(x * box_side + box_side // 10, y * box_side, (x + 1) * box_side - box_side // 10, (y + 1) * box_side, fill = "black")
+                player_one = game_canvas.create_rectangle(x * box_side + box_side // 10, y * box_side, (x + 1) * box_side - box_side // 10, (y + 1) * box_side, fill = "black", outline = "blue")
             elif level_map[y][x] == "p2":
                 game_canvas.create_rectangle(x * box_side, y * box_side, (x + 1) * box_side, (y + 1) * box_side, outline = "red", fill = "red")
-                player_two = game_canvas.create_rectangle(x * box_side + box_side // 10, y * box_side, (x + 1) * box_side - box_side // 10, (y + 1) * box_side, fill = "black")
+                player_two = game_canvas.create_rectangle(x * box_side + box_side // 10, y * box_side, (x + 1) * box_side - box_side // 10, (y + 1) * box_side, fill = "black", outline = "red")
 
-    bind_p1(game_canvas, level_map, player_one, player_two, box_side)
+    bind_p1()
+    print("bbbb")
+    game_canvas.focus_set()
     # movement_p2 = movement_class_p2(player_two)
 
     shirt_list = [0] * 10
@@ -57,11 +144,6 @@ def draw(level, root):
 
     shirt_yellow = PhotoImage(file = "../img/tricko_yellow.gif")
     # shirt_blue = PhotoImage(file = "../img/tricko_blue.gif")
-
-    id_1 = game_canvas.create_image(100, 100, image = shirt_yellow, anchor = NW)
-    id_2 = game_canvas.create_image(300, 300, image = shirt_yellow_2, anchor = NW)
-
-    game_canvas.move(id_1, 10, 0)
 
     print(game_canvas.coords(player_one))
     for shirt_count in range(10):
